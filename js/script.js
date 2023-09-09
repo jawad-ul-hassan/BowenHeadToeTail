8fc2f2ccea9275f863bffabacba6e0fcc432b1da
jawad-ul-hassan <intelligentjawad@gmail.com>
2023-09-09 23:00:01 +0500

    mobile version completed upto price list section

--- js/script.js
+++ js/script.js
@@ -1,15 +1,35 @@
+// Read More
+const readMoreBtn = document.querySelector('.about-mobile-more-btn');
+const readMoreText = document.querySelector('.about-mobile-more-btn-text');
+const arrowIcon = document.querySelector('.about-mobile-arrow');
+const aboutMobileMoreContent = document.querySelector(
+  '.about-mobile-more-content'
+);
+
+console.log(aboutMobileMoreContent);
+
+readMoreBtn.addEventListener('click', () => {
+  if (readMoreText.textContent === 'Read More') {
+    readMoreText.textContent = 'Read Less';
+    arrowIcon.style.transform = 'rotate(180deg)';
+    aboutMobileMoreContent.classList.remove('active');
+  } else {
+    readMoreText.textContent = 'Read More';
+    arrowIcon.style.transform = 'rotate(0deg)';
+    aboutMobileMoreContent.classList.add('active');
+  }
+});
+
+// Treatments Tabs
 function showTab(tabId) {
-  // Hide all tab content items
   const tabItems = document.querySelectorAll('.treatments-tab-item');
   tabItems.forEach(tabItem => tabItem.classList.remove('active'));
 
-  // Show the selected tab content
   const selectedTab = document.getElementById(tabId);
   selectedTab.classList.add('active');
 }
 
 function hideTab(tabId) {
-  // Hide the specified tab content
   const tabToHide = document.getElementById(tabId);
   tabToHide.classList.remove('active');
 }
